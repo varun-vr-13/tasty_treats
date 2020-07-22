390a787c8cd77bdeee727a0bb14b3b2ab633dff8
varun-vr-13 <varuncool495@gmail.com>
2020-07-22 21:25:07 +0530

    Updates

---
+++ precache-manifest.f46efc9060700784f30387fd230fc947.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "234dff6d61926074ece0bd929a9f1ee8",
+    "url": "/tasty_treats/index.html"
+  },
+  {
+    "revision": "0a9b1969fcf9261ea0b9",
+    "url": "/tasty_treats/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ea32e6097e83e22fb026",
+    "url": "/tasty_treats/static/js/2.3a42c244.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tasty_treats/static/js/2.3a42c244.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a9b1969fcf9261ea0b9",
+    "url": "/tasty_treats/static/js/main.f5e0f6bd.chunk.js"
+  },
+  {
+    "revision": "71903230671e03a33999",
+    "url": "/tasty_treats/static/js/runtime-main.5338bd33.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tasty_treats/static/media/logo.5d5d9eef.svg"
+  }
+]);
