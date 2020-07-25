6b509bc550e8fbf08df34a592e1a478e4f7861f4
varun-vr-13 <varuncool495@gmail.com>
2020-07-25 13:38:43 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import {useState,useEffect,useRef} from 'react';
+import hello from './main'
+
+
+import {HashRouter,Route, Switch} from "react-router-dom"
+//export const history = createHashHistory();
+
+function App(props) {
+  console.log(props);
+const[count,setCount] = useState('');
+const[counts,setCounts] = useState('');
+let interval = useRef();
+const loadd = ()=>{
+ setCount("asty");
+ setCounts("reats");
+
+}
+
+const red = () => {
+console.log(interval)
+}
+useEffect(()=>{
+const  interval = setInterval(()=>{
+loadd();
+
+},1000);
+
+
+  red();
+
+return()=>{
+  clearInterval(interval.current);
+}
+},[]);
+
   return (
+<HashRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       
+    <Switch>
+ 
+ <Route  path="/" exact component={App}></Route>
+         <Route path="/main" component={hello}></Route>
+      </Switch>
+       <img src="./pics/cake.svg" />
+        <span ><span className="id1">T<span className='div1'>{count}</span></span><span className="id2">T<span className='div1'>{counts}</span></span></span>
     </div>
+    </HashRouter>
   );
 }
+
 
 export default App;
